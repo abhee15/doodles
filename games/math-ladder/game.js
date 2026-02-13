c70c57e1a00e8b29607ac7e552ca69d861f85a42
abhee15 <abhee15@gmail.com>
2026-02-12 23:30:26 -0600

    Make Math Ladder fully responsive for mobile devices

--- games/math-ladder/game.js
+++ games/math-ladder/game.js
@@ -7,6 +7,12 @@ const config = {
     height: 600,
     parent: 'game-container',
     backgroundColor: '#87CEEB',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade: {
